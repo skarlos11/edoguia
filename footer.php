938c5a2570c31d98aae5eeef02a99c024d7ea00a
skarlos11 <skarlos1991@gmail.com>
2013-08-17 13:50:21 -0500

    footer margin change
    footer float changes

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <div id="footer">
-	<div class="textof">
+	<div class="textof span8">
 	<p><strong>Edo guía</strong> es un proyecto pensado para promover el turismo local a nivel nacional e internacional, además de
 	apoyar a los empresarios y comerciantes locales por medio de publicidad económica para sus servicios y establecimientos.
 	es gracias a dicha publicidad que este proyecto se mantiene.</p>
