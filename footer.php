50d1b9ccfab2a2f3098cc7f7854fcc54ff4c1b2f
skarlos11 <skarlos1991@gmail.com>
2013-08-16 19:11:01 -0500

    inicio de tema
    primera parte

---
+++ footer.php
@@ -0,0 +1,13 @@
+<div id="footer">
+	<div class="textof">
+	<p><strong>Edo guía</strong> es un proyecto pensado para promover el turismo local a nivel nacional e internacional, además de
+	apoyar a los empresarios y comerciantes locales por medio de publicidad económica para sus servicios y establecimientos.
+	es gracias a dicha publicidad que este proyecto se mantiene.</p>
+
+	
+	</div>	
+
+</div>
+
+
+</html>
---
+++ header.php
@@ -0,0 +1,26 @@
+<html>
+	<head>
+
+			<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>">
+
+	</head>
+
+	<div id="header">
+		
+		<div id="logo">
+			
+		</div>
+
+		<div >
+
+			<ul id="menu">
+				<li>Historia</li>
+				<li>Sociedad</li>
+				<li>Sitios de Interes</li>
+				<li>Directorio</li>
+				<li>Contacto</li>
+			</ul>
+
+		</div>
+
+	</div>
