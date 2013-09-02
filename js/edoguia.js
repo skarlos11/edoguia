e282bb8cc0a93eca4101f96d51246d4a88de49c0
Artemio Rodriguez Perez <isc.arodriguez@gmail.com>
2013-09-02 00:06:43 -0500

    Menu
    Menu

--- js/edoguia.js
+++ js/edoguia.js
@@ -4,4 +4,7 @@ jQuery(function($) {
 		window.location = $(this).attr('url');
 	});
 
+	var page_name = $('.pag-title').text();
+	$('.menu-item:contains("'+page_name+'")').addClass('current');
+
 });
