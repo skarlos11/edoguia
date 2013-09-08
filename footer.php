93ca07cfd94d377cd512a02ff21ac8983eee5e3e
skarlos11 <skarlos1991@gmail.com>
2013-09-08 17:21:56 -0500

    Footer changes
    because national and international tourism sounds like a superficial
    shit...

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
 		<div class="row">
 
 			<div class="textof span8">
-			<p><strong>Edo guía</strong> es un proyecto pensado para promover el turismo local a nivel nacional e internacional, además de
+			<p><strong>Edo guía</strong> es un proyecto pensado para promover el turismo local, además de
 			apoyar a los empresarios y comerciantes locales por medio de publicidad económica para sus servicios y establecimientos.
 			es gracias a dicha publicidad que este proyecto se mantiene.</p>
 
