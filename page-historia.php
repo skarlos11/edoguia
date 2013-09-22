4698bee13aa97a1731a75c83ef1211253f02a462
Artemio Rodriguez Perez <isc.arodriguez@gmail.com>
2013-09-22 18:06:12 -0500

    Historia Page
    Historia Page

---
+++ page-historia.php
@@ -0,0 +1,28 @@
+<?php get_header() ?>
+
+<div id="content" class="row">
+	
+	<?php echo do_shortcode('[rev_slider historia]'); ?>
+
+	<br />
+
+	<div class="span7" style="margin-left: 0px;">	    
+		    <?php if (have_posts()) : while (have_posts()) : the_post();?>
+		    <div class="info" >
+		        <h2 class="pag-title"><?php the_title();?></h2>
+		        <div>
+		            <?php the_content('<p class="serif">Read the rest of this page »</p>'); ?>
+		        </div>
+		    </div>
+		    <?php endwhile; endif; ?>
+	</div>
+	<div class="span4" >
+
+		<?php related_post(); ?>
+
+
+		<?php contacto_post(); ?>
+		
+	</div>
+</div>
+<?php get_footer() ?>
