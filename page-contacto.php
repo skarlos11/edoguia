db9949c8e6e032bbe2335f83aea5a9cb0e4b04b3
Artemio Rodriguez Perez <isc.arodriguez@gmail.com>
2013-09-22 17:25:20 -0500

    Contacto Page
    Contacto Page

--- page-contacto.php
+++ page-contacto.php
@@ -1,19 +1,15 @@
 <?php get_header() ?>
 
-	<div id="content" class="row">
-
-		<div class="title-cat span12" style="margin-left: 0px" ><h2><?php page_title(); ?></h2></div>
-	 		<?php if (have_posts()) : while (have_posts()) : the_post();?>
-		    <div class="info" >
-		        <h2 class="pag-title"><?php the_title();?></h2>
+<div id="content" class="row">
+	<div class="span12" style="margin-left: 0px;">	    
+		    <?php if (have_posts()) : while (have_posts()) : the_post();?>
+		    <div  >
+		        <h2 ><?php the_title();?></h2>
 		        <div>
 		            <?php the_content('<p class="serif">Read the rest of this page »</p>'); ?>
 		        </div>
 		    </div>
 		    <?php endwhile; endif; ?>
-		</div>
-
 	</div>
-
-
+</div>
 <?php get_footer() ?>
